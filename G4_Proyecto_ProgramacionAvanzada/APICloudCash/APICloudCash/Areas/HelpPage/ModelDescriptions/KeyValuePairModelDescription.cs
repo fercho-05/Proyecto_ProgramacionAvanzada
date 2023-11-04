b52f474a97653413385d3befeff08d70dc5ac3db
Jarod Santiago Viquez Salazar <jarod281@hotmail.com>
2023-11-04 13:18:42 -0600

    Creacion de la API
    Funciones creadas:
    IngresarCliente
    IngresarAdministrador
    ObtenerTipoUsuarios
    ObtenerTipoDivisas
    ListarClientes
    IngresarTarjetaDebito
    IngresarTarjetaCredito
    IniciarSesion

---
+++ G4_Proyecto_ProgramacionAvanzada/APICloudCash/APICloudCash/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace APICloudCash.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ G4_Proyecto_ProgramacionAvanzada/APICloudCash/APICloudCash/Entities/Utilitario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+
+namespace APICloudCash.Entities
+{
+    public class Utilitario
+    {
+        
+        
+        string correoUsuario = ConfigurationManager.AppSettings["CorreoUsuario"];
+        string claveUsuario = ConfigurationManager.AppSettings["ClaveUsuario"];
+        string servidorCorreo = ConfigurationManager.AppSettings["ServidorCorreo"];
+        /*
+        public void EnvioCorreos(string destino, string asunto, string contenido)
+        {
+
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(correoUsuario);
+            mailMessage.To.Add(new MailAddress(destino));
+            mailMessage.Subject = asunto;
+            mailMessage.Body = contenido;
+            mailMessage.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient();// Servidor de correo
+            smtpClient.Port = 587;
+            smtpClient.Host = servidorCorreo;
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(correoUsuario, claveUsuario);
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.Send(mailMessage);
+        }*/
+    }
+
+}
