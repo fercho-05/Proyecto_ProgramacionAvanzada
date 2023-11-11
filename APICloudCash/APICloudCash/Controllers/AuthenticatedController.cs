88177f7ddc1f35b2db7ebff7e4bb5bc361819223
Tannya Granados <101310816+tannyagrana2@users.noreply.github.com>
2023-11-11 17:16:42 -0600

    adicion de pantalla para ver el perfil del usuario

---
+++ APICloudCash/APICloudCash/Controllers/AuthenticatedController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APICloudCash.Controllers
+{
+    public class AuthenticatedController : Controller
+    {
+        // GET: Authenticated
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
--- WEBCloudCash/G4_Proyecto_ProgramacionAvanzada/Controllers/LoginController.cs
+++ WEBCloudCash/G4_Proyecto_ProgramacionAvanzada/Controllers/LoginController.cs
@@ -28,6 +28,13 @@ namespace G4_Proyecto_ProgramacionAvanzada.Controllers
             if (resp != null)
             {
                 Session["CodigoUsuario"] = resp.id_Usuario;
+                Session["CedulaUsuario"] = resp.cedula;
+                Session["userName"] = resp.nombreUsuario;
+                Session["NombreUsuario"] = resp.nombre;
+                Session["Apellido1Usuario"] = resp.apellidoUno;
+                Session["Apellido2Usuario"] = resp.apellidoDos;
+                Session["TelefonoUsuario"] = resp.telefono;
+                Session["CorreoUsuario"] = resp.correo;
                 Session["NombreCompleto"] = resp.nombre +" "+ resp.apellidoUno + " "+ resp.apellidoDos;
                 return RedirectToAction("Perfil", "Authenticated");
             }
