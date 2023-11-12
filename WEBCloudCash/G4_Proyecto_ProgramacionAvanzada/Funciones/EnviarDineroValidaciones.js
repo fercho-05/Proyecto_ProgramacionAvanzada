5db054cc06f4f0fed265d02c9236815f593124ee
Tannya Granados <101310816+tannyagrana2@users.noreply.github.com>
2023-11-12 17:32:19 -0600

    vista pagar servicios, js de vista pagar servicios

---
+++ WEBCloudCash/G4_Proyecto_ProgramacionAvanzada/Funciones/EnviarDineroValidaciones.js
@@ -0,0 +1,33 @@
+﻿
+$(document).ready(function () {
+
+    $("#tipoCuenta").change(function () {
+        var seleccion = $(this).val();
+        // MOSTRAR DEPENDIENDO DE LA CUENTA
+        $("#datosCostarricense").toggle(seleccion === "costarricense");
+        $("#nombreCompletoInternacional").toggle(seleccion === "internacional");
+        $("#montoTransferirContainer").toggle(true); //UNA VEZ SE ELIGE TIPO DE CUENTA SE MUESTRAN CAMPOS ASUNTO Y MONTO
+        $("#asuntoContainer").toggle(true);
+        $("#asuntoContainer").toggle(true);
+        $("#botonTransfiere").toggle(true);
+        $("#cuentaIban").toggle(true);
+    });
+
+    // VALIDA FORMATO CUENTA IBAN: DEBE EMPEZAR POR DOS LETRAS Y LUEGO HASTA 32 NUMEROS
+    
+    var valorOriginal; //AQUI SE GUARDAN LAS LETRAS PARA LUEGO SEGUIR CON LSO NUMEROS
+     
+    $("#ibanInput").on('input', function () {
+        var ibanInput = $(this).val();
+        var formatoIBAN = /^[A-Za-z]{2}[0-9]+$/;
+
+        if (ibanInput.length <= 2) {
+            // SOLO PERMITE DOS LETRAS
+            valorOriginal = ibanInput.replace(/[^a-zA-Z]/g, '').toUpperCase();
+        } else {
+            // LUEGO DE INGRESAR LAS LETRAS DEL PAIS YA SOLO PERMITIR NUMS
+            valorOriginal = valorOriginal.substring(0, 2) + ibanInput.substring(2,34).replace(/[^0-9]/g, '');
+        }
+        $(this).val(valorOriginal);       
+    });
+});
---
+++ WEBCloudCash/G4_Proyecto_ProgramacionAvanzada/Funciones/ValidacionesServicios.js
@@ -0,0 +1,21 @@
+﻿
+$(document).ready(function () {
+
+    $("#tipoServicio").change(function () {
+        var seleccion = $(this).val();
+        // MOSTRAR DEPENDIENDO DE LA CUENTA
+        $("#pagoCelular").toggle(seleccion === "cel");
+        $("#pagoInternet").toggle(seleccion === "wifi");
+        $("#pagoLuz").toggle(seleccion === "light");
+        $("#pagoTV").toggle(seleccion === "tv");
+    });
+ 
+});
+
+function predefinirPrefijo() {
+    var telefonoInput = document.getElementById("TELEFONO");
+
+    if (!telefonoInput.value.startsWith("+506")) {
+        telefonoInput.value = "+506" + telefonoInput.value;
+    }
+}
