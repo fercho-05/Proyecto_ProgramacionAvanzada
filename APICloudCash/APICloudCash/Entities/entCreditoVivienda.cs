cb16c9d32e8662836bc8d65b47f7c5b198c4ed18
Tannya Granados <101310816+tannyagrana2@users.noreply.github.com>
2023-12-13 10:47:09 -0600

    metodo en el api para obtener credito de vivienda por cédula, actualizacion script bd con tabla credito vivienda

---
+++ APICloudCash/APICloudCash/Entities/entCreditoVivienda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APICloudCash.Entities
+{
+    public class entCreditoVivienda
+    {
+        public long id_CreditoVivienda { get; set; }
+        public long id_Cliente { get; set; }
+        public int PorcentajeInteres { get; set; }
+        public int PlazoAnnios { get; set; }
+        public DateTime FechaAprobacion { get; set; }
+
+    }
+}
