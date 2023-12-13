315b638172eb0ba929dade929d6cd7ad43bb1fae
Tannya Granados <101310816+tannyagrana2@users.noreply.github.com>
2023-12-12 22:52:49 -0600

    base de datos con tabla de envioDinero

--- BD_CloudCash.sql
+++ BD_CloudCash.sql
@@ -12,6 +12,37 @@ CREATE TABLE [dbo].[Administradores](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+
+/****** Object:  Table [dbo].[EnvioDinero]    Script Date: 12/12/2023 10:51:19 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[EnvioDinero](
+	[id_EnvioDinero] [bigint] NOT NULL,
+	[id_Cuenta] [bigint] NOT NULL,
+	[nombreReceptor] [varchar](70) NOT NULL,
+	[numeroCuentaReceptor] [varchar](30) NOT NULL,
+	[monto] [bigint] NOT NULL,
+	[asunto] [varchar](15) NOT NULL,
+ CONSTRAINT [PK_EnvioDinero] PRIMARY KEY CLUSTERED 
+(
+	[id_EnvioDinero] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[EnvioDinero]  WITH CHECK ADD  CONSTRAINT [FK_Cuentas_EnvioDinero] FOREIGN KEY([id_Cuenta])
+REFERENCES [dbo].[Cuentas] ([id_Cuenta])
+GO
+
+ALTER TABLE [dbo].[EnvioDinero] CHECK CONSTRAINT [FK_Cuentas_EnvioDinero]
+GO
+
+
 /****** Object:  Table [dbo].[Clientes]    Script Date: 12/12/2023 16:29:25 ******/
 CREATE TABLE [dbo].[Clientes](
 	[id_Cliente] [bigint] NOT NULL,
